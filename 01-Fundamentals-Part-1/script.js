247b9507b93951b8d8645181845c62aeb1b0376c
leaimy <hantctk42@gmail.com>
2023-08-09 09:30:50 +0700

    let, const and var 🍀

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -24,7 +24,7 @@ let job2 = "teacher";
 
 console.log(myFirstJob);
 */
-
+/*
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
@@ -44,3 +44,17 @@ console.log(typeof year);
 year = 1991;
 console.log(typeof year);
 console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schmedtmann";
+console.log(lastName);
